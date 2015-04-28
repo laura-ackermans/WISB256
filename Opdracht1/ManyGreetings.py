cbc8fe959d096cecc5fd9f3f9f3640aa580e09ec
Laura Ackermans <lje.ackermans@gmail.com>
2015-04-28 10:04:38 +0000

    Nu echt klaar

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 name = input("What is your name? ");
-print("Hello, " + name + "!");
-=======
-name = input("Name? ");
 numberofgreetings = input("How many greetings? ");
 print(numberofgreetings + " x Hello, " + name + "!");
->>>>>>> extension
