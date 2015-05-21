e1fd46bae90a29b6bd29d767e57bbb08301160ef
Laura Ackermans <lje.ackermans@gmail.com>
2015-05-21 13:01:06 +0000

    EerstePoging

---
+++ Opdracht4/bisection.py
@@ -0,0 +1,21 @@
+def findRoot(f,a,b,epsilon): 
+    m = (a+b)/2
+    if abs(a - b) <= epsilon: 
+        return m
+    elif f(a) == 0: 
+        return a 
+    elif f(b) == 0: 
+        return b
+    elif f(m) == 0: 
+        return m
+    else:
+        if f(a) > 0 and f(m) < 0: 
+            return findRoot(f,a,m,epsilon)
+        elif f(a) < 0 and f(m) > 0: 
+            return findRoot(f,a,m,epsilon)
+        elif f(m) < 0 and f(b) > 0: 
+            return findRoot(f,m,b,epsilon)
+        elif f(m) > 0 and f(b) < 0: 
+            return findRoot(f,m,b,epsilon)
+        else: 
+            return 'no root'
