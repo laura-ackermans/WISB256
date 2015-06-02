5977f18479a1e6de4a06ef7556e0a1beb4acf654
Laura Ackermans <lje.ackermans@gmail.com>
2015-06-02 10:07:35 +0000

    Lorenz zonder bonus

---
+++ Opdracht6/lorenz.py
@@ -0,0 +1,32 @@
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axed3D as axes
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy
+import math
+
+class Lorenz: 
+    
+    sigma = 10
+    rho = 28
+    beta = 8/3
+    
+    def __init__(self,initial):
+        self.x = initial[0]
+        self.y = initial[1]
+        self.z = initial[2]
+        self.initial = initial
+        
+    def func(self, stap, t): 
+        xt = stap[0]
+        yt = stap[1]
+        zt = stap[2]
+        f0 = self.sigma*(yt - xt)
+        f1 = xt*(self.rho - zt) - yt
+        f2 = xt*yt - self.beta*zt
+        return [f0, f1, f2]
+ 
+    def solve(self, T,dt): 
+        t = numpy.arange(0,T,dt)
+        solution = odeint(self.func, self.initial, t)
+        return solution
